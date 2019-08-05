607ab29ed38e9fadd1a2696c828b0f3fafdbf2a9
kkyb123 <kouokamkuisu123@gmail.com>
2019-08-05 17:36:22 +0100

    ALC4 | Yohane | Design and implement ui for the insert activity

--- app/src/main/java/com/kkyb/travelmatics/InsertActivity.java
+++ app/src/main/java/com/kkyb/travelmatics/InsertActivity.java
@@ -1,16 +1,11 @@
 package com.kkyb.travelmatics;
 
 import android.os.Bundle;
-
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 
 public class InsertActivity extends AppCompatActivity {
 
@@ -20,21 +15,12 @@ public class InsertActivity extends AppCompatActivity {
     setContentView(R.layout.activity_insert);
     Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
-
-    FloatingActionButton fab = findViewById(R.id.fab);
-    fab.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-            .setAction("Action", null).show();
-      }
-    });
   }
 
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
     // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_main, menu);
+    getMenuInflater().inflate(R.menu.save_menu, menu);
     return true;
   }
 
@@ -44,11 +30,6 @@ public class InsertActivity extends AppCompatActivity {
     // automatically handle clicks on the Home/Up button, so long
     // as you specify a parent activity in AndroidManifest.xml.
     int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
 
     return super.onOptionsItemSelected(item);
   }
